2e58730bc27c133ebba03b87d726c3128c9c6e20
Sam Cranford <me@nostradamnit.com>
2024-02-28 19:04:16 +0100

    We can say Buzz for 5 :D

--- Sources/FizzBuzz.swift
+++ Sources/FizzBuzz.swift
@@ -1,7 +1,10 @@
 class FizzBuzz{
     func say(number: Int) -> String {
-        if(number % 3 == 0) {
+        if (number % 3 == 0) {
             return "Fizz"
+        }
+        if (number % 5 == 0) {
+            return "Buzz"
         }
         return String(number)
     }
